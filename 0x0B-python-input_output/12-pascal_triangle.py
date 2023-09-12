32d3c41f972fa8328efea805d5bc7f0d28785d3e
Spesho-clei <jepkoechnancy56@gmail.com>
2023-09-12 07:27:01 +0000

    Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""pascal_traigle module."""
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    while len(triangle) < n:
+        row = [1]
+        prev_row = triangle[-1]
+        for i in range(1, len(prev_row)):
+            row.append(prev_row[i-1] + prev_row[i])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
