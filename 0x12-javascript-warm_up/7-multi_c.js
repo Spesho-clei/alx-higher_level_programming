2d6d4409df07ef618f322b1bdd8e54f86bbb5576
Spesho-clei <jepkoechnancy56@gmail.com>
2023-11-13 01:18:30 -0800

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// Get the first argument
+const arg = process.argv[2];
+
+// Convert the argument to an integer
+const numOccurrences = parseInt(arg);
+
+// Check if the conversion is successful
+if (!isNaN(numOccurrences)) {
+  // Loop to print "C is fun" x times
+  for (let i = 0; i < numOccurrences; i++) {
+    console.log("C is fun");
+  }
+} else {
+  console.log("Missing number of occurrences");
+}
