1833b8a5fc7a31f6100ad0a187f843951868e1f3
Spesho-clei <jepkoechnancy56@gmail.com>
2023-11-13 01:35:06 -0800

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 // Array of strings
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 // Loop through the array and print each line
 for (let i = 0; i < lines.length; i++) {
