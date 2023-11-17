9f1329de1b9cea714a7918370eb4a19fe4cda096
Spesho-clei <jepkoechnancy56@gmail.com>
2023-11-17 06:32:18 -0800

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
 
+""" Write a script that lists all State objects """
+
 if __name__ == "__main__":
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-import sys
-from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy import create_engine
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy.orm import sessionmaker
+    import sys
+    from model_state import Base, State
 
-    i = sys.argv
-    if len(i) < 4:
+    inp = sys.argv
+    if len(inp) < 4:
         exit(1)
-
-    # Database connection
     conn_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-    engine = create_engine(conn_str.format(i[1], i[2], i[3]))
+    engine = create_engine(conn_str.format(inp[1], inp[2], inp[3]))
+    Session = sessionmaker(bind=engine)
+
     Base.metadata.create_all(engine)
 
-    # Create a session
-    Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Fetch all State objects and display them
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
+    output = session.query(State).order_by(State.id).all()
+    for state in output:
         print("{}: {}".format(state.id, state.name))
 
     session.close()
