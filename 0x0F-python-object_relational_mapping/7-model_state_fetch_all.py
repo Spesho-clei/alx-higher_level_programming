0d97106477af30d083afaa54c8685c412b4e45a3
Spesho-clei <jepkoechnancy56@gmail.com>
2023-11-17 06:07:57 -0800

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+import sys
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
     # Database connection
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, database),
-                           pool_pre_ping=True)
+    conn_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(conn_str.format(username, password, database), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     # Create a session
